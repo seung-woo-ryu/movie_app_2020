278ca2b0cbdfb67ff10408490422d8c79deb3a76
Ryu <tmddn645@naver.com>
2020-07-19 15:51:09 +0900

    state 2th course

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends React.Component{
   };
   
 
-  add = () =>{
-    console.log("add");
+  add = () => {
+    this.setState(current => ({ count: current.count + 1 }));
   };
   
   minus = () =>{
-    console.log("minus");
+    this.setState(current => ({ count: current.count - 1 }));
   };
 
   render(){
     return(
       <div>
         <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add()}>Add</button>
-        <button onClick={this.minus()}>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     );
   }
