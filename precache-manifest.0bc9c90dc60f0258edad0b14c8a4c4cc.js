9413329fe89930f0567e0c6f603bbffadb7a83a7
Ryu <tmddn645@naver.com>
2020-07-30 01:01:57 +0900

    Updates

---
+++ precache-manifest.0bc9c90dc60f0258edad0b14c8a4c4cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fadbe5f618f27d8d810bbac91efd3b0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "fdd80b7e664a5a940d3e",
+    "url": "/movie_app_2020/static/css/main.c5588f7a.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app_2020/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdd80b7e664a5a940d3e",
+    "url": "/movie_app_2020/static/js/main.93bd6cad.chunk.js"
+  },
+  {
+    "revision": "4839ee59ec104f2d85b3",
+    "url": "/movie_app_2020/static/js/runtime-main.2f3c173d.js"
+  }
+]);
